a50e29511186bf228de8f0cfa9801bd97d57b9b8
Nurdaulet <nurdaulet.beketov.2005@gmail.com>
2025-06-07 18:48:22 +0500

    prod deployment the another test

--- src/main/java/com/discount_backend/Discount_backend/HelloController.java
+++ src/main/java/com/discount_backend/Discount_backend/HelloController.java
@@ -1,11 +1,10 @@
 package com.discount_backend.Discount_backend;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/")
     public String home() {
-        return "Hello, Spring Boot is running!";
+        return "Hello, Spring Boot is running! And the discount service is up and running!";
     }
 }
